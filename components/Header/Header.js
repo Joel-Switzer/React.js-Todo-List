71f340482ce13085c532dcb897ff819399f3b659
Joel Switzer <Joelswitzer1@gmail.com>
2019-02-18 18:39:47 -0600

    Initial commit

---
+++ components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+// Functional component for header
+function Header() {
+    return (
+        <header className='head'>
+            <h1>To-do List</h1>
+        </header>
+    )
+}
+
+export default Header
